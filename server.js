3ae1ea0066490b548a029f722c447ceb978d1980
peter.chen1024 <u0052017@gmail.com>
2022-04-20 14:29:46 +0800

    update cloud atlas connection

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const http = require('http');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const {HEADERS, REQUEST_METHOD} = require('./methods/constant');
 const {successHandle, errorHandle} = require('./methods/httpHandle');
 const Post = require('./models/post');
 
+// 載入 env
+dotenv.config({path:'./config.env'});
+
 // 資料庫連線
-mongoose.connect('mongodb://localhost:27017/todolist')
+const DBUrl = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD)
+mongoose.connect(DBUrl)
 .then(()=>{
   console.log('資料庫連線成功');
 }).catch((err)=>{
@@ -88,4 +93,4 @@ const requestListener = async (req, res) =>{
 
 // server 監聽
 const server = http.createServer(requestListener);
-server.listen(3005);
+server.listen(process.env.PORT || 3005);
