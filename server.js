e544e89d826331d89853ac5df241b42f284f5937
peter.chen1024 <u0052017@gmail.com>
2022-04-20 12:48:51 +0800

    add schema, model

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const http = require('http');
+const mongoose = require('mongoose');
 const {HEADERS, REQUEST_METHOD} = require('./methods/constant');
 const {successHandle, errorHandle} = require('./methods/httpHandle');
+const Post = require('./models/post');
+
+// 資料庫連線
+mongoose.connect('mongodb://localhost:27017/todolist')
+.then(()=>{
+  console.log('資料庫連線成功');
+}).catch((err)=>{
+  console.log(err);
+})
+
+
 
 const requestListener = (req, res) =>{
   
