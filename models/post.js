cdc9b15a823d192c0587647007b3225c7a5f7a41
peter.chen1024 <u0052017@gmail.com>
2022-04-22 14:07:49 +0800

    update posts model field

--- models/post.js
+++ models/post.js
@@ -1,24 +1,51 @@
-const {Schema, model} = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 // Schema
 const postSchema = new Schema(
   {
-    title:{
+    name: {
       type: String,
-      required: [true, '標題必填']
+      required: [true, "姓名未填寫"],
     },
-    createdAt:{
+    content: {
+      type: String,
+      required: [true, "內容未填寫"],
+    },
+    image: {
+      type: String,
+      default: null,
+    },
+    tags: [
+      {
+        type: String,
+        required: [true, "標籤未填寫"],
+      },
+    ],
+    type: {
+      type: String,
+      enum: ["person", "group"],
+      required: [true, "貼文類型未填寫"],
+    },
+    likes: {
+      type: Number,
+      default: 0,
+    },
+    comments: {
+      type: Number,
+      default: 0,
+    },
+    createdAt: {
       type: Date,
       default: Date.now,
-      select: false
-    }
+      select: false,
+    },
   },
   {
     versionKey: false,
   }
-)
+);
 
 // model
-const Post = new model('Post', postSchema);
+const Post = new model("Post", postSchema);
 
 module.exports = Post;
--- server.js
+++ server.js
@@ -1,95 +1,120 @@
-const http = require('http');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const {HEADERS, REQUEST_METHOD} = require('./methods/constant');
-const {successHandle, errorHandle} = require('./methods/httpHandle');
-const Post = require('./models/post');
+const http = require("http");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const { HEADERS, REQUEST_METHOD } = require("./methods/constant");
+const { successHandle, errorHandle } = require("./methods/httpHandle");
+const Post = require("./models/post");
 
 // 載入 env
-dotenv.config({path:'./config.env'});
+dotenv.config({ path: "./config.env" });
 
 // 資料庫連線
-const DBUrl = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD)
-mongoose.connect(DBUrl)
-.then(()=>{
-  console.log('資料庫連線成功');
-}).catch((err)=>{
-  console.log(err);
-})
+const DBUrl = process.env.DATABASE.replace(
+  "<password>",
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DBUrl)
+  .then(() => {
+    console.log("資料庫連線成功");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-const requestListener = async (req, res) =>{
+const requestListener = async (req, res) => {
+  let body = "";
+  req.on("data", (chunk) => (body += chunk)); // 接收資料
 
-  let body = '';
-  req.on('data', chunk => body+=chunk); // 接收資料
-  
-  if(req.url === '/posts' && req.method === REQUEST_METHOD.GET){
+  if (req.url === "/posts" && req.method === REQUEST_METHOD.GET) {
     const posts = await Post.find();
     successHandle(res, posts);
-  } else if(req.url === '/posts' && req.method === REQUEST_METHOD.POST){
-    req.on('end', async ()=>{
-      try{
+  } else if (req.url === "/posts" && req.method === REQUEST_METHOD.POST) {
+    req.on("end", async () => {
+      try {
         const data = JSON.parse(body);
-        const { title } = data;
-        if(title !== undefined){
+        const { name, content, image, tags, type, likes, comments } = data;
+
+        // 前端阻擋 - 欄位格式不正確
+        if (name === undefined) {
+          errorHandle(res, "姓名未填寫");
+        } else if (content === undefined) {
+          errorHandle(res, "內容未填寫");
+        } else if (tags === undefined) {
+          errorHandle(res, "標籤未填寫");
+        } else if (type === undefined) {
+          errorHandle(res, "貼文類型未填寫");
+        } else {
           // 新增資料
           const post = await Post.create({
-            title
-          })
+            name,
+            content,
+            image,
+            tags,
+            type,
+            likes,
+            comments,
+          });
           const posts = await Post.find();
           successHandle(res, posts);
-        } else {
-          errorHandle(res, '標題必填')
         }
-      }catch(err){
+      } catch (err) {
         errorHandle(res, err);
       }
-    })
-  } else if(req.url === '/posts' && req.method === REQUEST_METHOD.DELETE){
+    });
+  } else if (req.url === "/posts" && req.method === REQUEST_METHOD.DELETE) {
     await Post.deleteMany({});
     const posts = await Post.find();
     successHandle(res, posts);
-  } else if(req.url.startsWith('/posts/') && req.method === REQUEST_METHOD.DELETE){
-    const id = req.url.split('/').pop();
+  } else if (
+    req.url.startsWith("/posts/") &&
+    req.method === REQUEST_METHOD.DELETE
+  ) {
+    const id = req.url.split("/").pop();
     const post = await Post.findByIdAndDelete(id);
     const posts = await Post.find();
-    if(post !== null){
+    if (post !== null) {
       successHandle(res, posts); // 單筆刪除成功
     } else {
-      errorHandle(res, '查無此 id'); // 查無 id
+      errorHandle(res, "查無此 id"); // 查無 id
     }
-  } else if(req.url.startsWith('/posts/') && req.method === REQUEST_METHOD.PATCH){
-    req.on('end', async ()=>{
-      try{
+  } else if (
+    req.url.startsWith("/posts/") &&
+    req.method === REQUEST_METHOD.PATCH
+  ) {
+    req.on("end", async () => {
+      try {
         const data = JSON.parse(body);
-        const id = req.url.split('/').pop();
-        const { title } = data;
+        const id = req.url.split("/").pop();
+        const { name, content, image, tags, type, likes, comments } = data;
 
-        if( title !== undefined && title !== ""){
-          const post = await Post.findByIdAndUpdate(id,{
-            title
-          })
-          const posts = await Post.find();
-          if(post !== null){
-            successHandle(res, posts);
-          } else{
-            errorHandle(res, '查無此 id'); // 查無 id
-          }
+        const post = await Post.findByIdAndUpdate(id, {
+          name,
+          content,
+          image,
+          tags,
+          type,
+          likes,
+          comments,
+        });
+        const posts = await Post.find();
+        if (post !== null) {
+          successHandle(res, posts);
         } else {
-          errorHandle(res, '標題必填');
+          errorHandle(res, "查無此 id"); // 查無 id
         }
-      } catch (err){
+      } catch (err) {
         errorHandle(res, err);
       }
-    })
-  
-  } else if(req.method === REQUEST_METHOD.OPTIONS){
+    });
+  } else if (req.method === REQUEST_METHOD.OPTIONS) {
     res.writeHead(200, HEADERS);
     res.end();
   } else {
     // 404 Page
-    errorHandle(res, '無此網站路由', 404);
+    errorHandle(res, "無此網站路由", 404);
   }
-}
+};
 
 // server 監聽
 const server = http.createServer(requestListener);
